7a37872aa2631d6d77c3e01edefb1c685c06ef12
Yosef-ahmed13 <ya3483652@gmail.com>
2026-04-10 04:30:00 +0200

    Deploying jeo_js pipeline files

--- main.go
+++ main.go
@@ -138,26 +138,14 @@ func main() {
 }
 
 func fetchLines(input string) ([]string, error) {
-	var scanner *bufio.Scanner
-	var err error
-
-	if strings.HasPrefix(input, "http://") || strings.HasPrefix(input, "https://") {
-		resp, err := http.Get(input)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-		scanner = bufio.NewScanner(resp.Body)
-	} else {
-		file, err := os.Open(input)
-		if err != nil {
-			return nil, err
-		}
-		defer file.Close()
-		scanner = bufio.NewScanner(file)
+	resp, err := http.Get(input)
+	if err != nil {
+		return nil, err
 	}
+	defer resp.Body.Close()
 
 	var lines []string
+	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
 		lines = append(lines, strings.TrimSpace(scanner.Text()))
 	}
